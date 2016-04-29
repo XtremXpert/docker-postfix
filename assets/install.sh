686fde6fa7f5f54c7e255b4e0c97f7d12115eebb
XtremXpert <benoit@vezina.biz>
2016-04-29 08:39:32 -0400

    Update install.sh

--- assets/install.sh
+++ assets/install.sh
@@ -40,12 +40,11 @@ postconf -e 'inet_protocols = ipv4'
 postconf -e 'myhostname = poste.xtremxpert.com'
 postconf -e 'mydomain = xtremxpert.com'
 postconf -e 'myorigin = xtremxpert.com'
-postconf -e 'mydestination = poste.xtremxpert.com, xtremxpert.com, localhost, localhost.localdomain'
+postconf -e 'mydestination = localhost'
 postconf -e 'mynetworks = 127.0.0.0/8,192.99.24.64/28'
-postconf -e 'relay_domains = xtremxpert.com'
 postconf -e 'mailbox_size_limit = 0'
 postconf -e 'recipient_delimiter = +'
-postconf -e 'virtual_transport = lmtp:dovecot:10026'
+postconf -e 'virtual_transport = virtual'
 postconf -e 'message_size_limit = 134217728'
 ############
 # MySQL
@@ -139,9 +138,12 @@ EOF
 # Cyrus-SASL support for authentication of mail clients.
 ############
 # /etc/postfix/main.cf
-#postconf -e smtpd_sasl_auth_enable=yes
+postconf -e 'smtpd_tls_auth_only = yes'
+postconf -e 'smtpd_sasl_type = dovecot'
+postconf -e 'smtpd_sasl_path = private/auth'
+postconf -e 'smtpd_sasl_auth_enable=yes'
 #postconf -e broken_sasl_auth_clients=yes
-#postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated, permit_mynetworks, reject_unauth_destination'
 # smtpd.conf
 #cat >> /etc/postfix/sasl/smtpd.conf <<EOF
 #pwcheck_method: auxprop
